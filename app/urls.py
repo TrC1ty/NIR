dd6f490f28be7b8edc94eba7faa044acbe379662
Titov Pavel <ptitov@1d61.com>
2023-01-04 16:14:55 +0300

    Add forms and models

--- app/urls.py
+++ app/urls.py
@@ -13,12 +13,11 @@ urlpatterns = [
     # for projects
     path('projects/create', ProjectView.as_view(), name='project-create'),
     path('projects/index', ProjectView.index, name='project-index'),
-    path('projects/project/<int:value>', ProjectView.view, name='project-view'),
+    path('projects/<int:value>', ProjectView.view, name='project-view'),
 
     # for participants
     path('participants/index', ParticipantView.as_view(), name='participant-index'),
-    path('participants/create', ParticipantView.post, name='participant-create'),
-    path('participants/add', ParticipantView.post, name='participant-add'),
+    path('projects/<int:project_id>/add/<str:participant>', ParticipantView.create, name='participant-create'),
 
     path('create-work', WorkView.as_view(), name='create-work'),
     path('create-material', MaterialView.as_view(), name='create-material'),
--- app/views/ParticipantView.py
+++ app/views/ParticipantView.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.shortcuts import render
 from ..models.ParticipantModel import ParticipantModel
 from ..forms.ParticipantForm import ParticipantForm
+from ..models.ProjectModel import ProjectModel
 
 
 class ParticipantView(View):
@@ -13,7 +14,39 @@ class ParticipantView(View):
         return render(request, 'participants/index.html', {'participants': participants})
 
     @staticmethod
+    def create(request: HttpRequest, project_id, participant) -> HttpResponse:
+        form = ParticipantForm()
+        form.project_id = project_id
+        form.project_field = participant
+
+        return render(request, 'participants/creation.html', {'form': form})
+
+    @staticmethod
     def post(request: HttpRequest) -> HttpResponse:
         form = ParticipantForm(request.POST)
         if form.is_valid():
+            project_id = form.cleaned_data["project_id"]
+            project = ProjectModel.objects.get(id=project_id)
+
+            participant = ParticipantModel.objects.create(
+                participant_type=form.cleaned_data["participant_type"],
+                subject_type=form.cleaned_data["subject_type"],
+                surname=form.cleaned_data["surname"],
+                name=form.cleaned_data["name"],
+                patronymic=form.cleaned_data["patronymic"],
+                post=form.cleaned_data["post"],
+                passport_data=form.cleaned_data["passport_data"],
+                register_of_specialists=form.cleaned_data["register_of_specialists"],
+                ogrn=form.cleaned_data["ogrn"],
+                inn=form.cleaned_data["inn"],
+                address=form.cleaned_data["address"],
+                phone=form.cleaned_data["phone"],
+                legal_name=form.cleaned_data["legal_name"],
+                details_admin_doc=form.cleaned_data["details_admin_doc"]
+            )
+            participant.save()
+            project_field = form.cleaned_data['project_field']
+            project[f"{project_field}"] = participant
+            project.save()
+
             return render(request, 'participants/creation.html', {'form': form})
