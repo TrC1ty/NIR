02730300d5122b6e0741d20fdeb38d9bf608e58b
Titov Pavel <ptitov@1d61.com>
2023-01-08 00:50:00 +0300

    Add edit participant

--- app/forms/ParticipantForm.py
+++ app/forms/ParticipantForm.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.forms import ModelForm, Textarea, TextInput, NumberInput, DateInput, Select
+from app.models.ParticipantModel import ParticipantModel
 
 SubjectType = (
     ('ЮЛ', 'Юридическое лицо'),
@@ -75,7 +76,7 @@ class ParticipantForm(forms.Form):
         required=False,
     )
     phone = forms.CharField(
-        label='Телефон',
+        label='Номер телефона',
         widget=forms.TextInput(attrs={'class': 'form-control'}),
         required=False,
     )
@@ -91,5 +92,42 @@ class ParticipantForm(forms.Form):
         required=False,
     )
 
-    project_id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
-    project_field = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+class Participant(ModelForm):
+    class Meta:
+        model = ParticipantModel
+        fields = ['surname', 'name', 'patronymic', 'post', 'passport_data', 'register_of_specialists', 'ogrn', 'inn',
+                  'address', 'phone', 'legal_name', 'details_admin_doc', 'participant_type', 'subject_type']
+        widgets = {
+            'surname': TextInput(attrs={'class': 'form-control'}),
+            'name': TextInput(attrs={'class': 'form-control'}),
+            'patronymic': TextInput(attrs={'class': 'form-control'}),
+            'post': TextInput(attrs={'class': 'form-control'}),
+            'passport_data': TextInput(attrs={'class': 'form-control'}),
+            'register_of_specialists': NumberInput(attrs={'class': 'form-control'}),
+            'ogrn': TextInput(attrs={'class': 'form-control'}),
+            'inn': TextInput(attrs={'class': 'form-control'}),
+            'address': TextInput(attrs={'class': 'form-control'}),
+            'phone': DateInput(attrs={'class': 'form-control'}),
+            'legal_name': TextInput(attrs={'class': 'form-control'}),
+            'details_admin_doc': TextInput(attrs={'class': 'form-control'}),
+            'participant_type': Select(choices=ParticipantType, attrs={'class': 'form-select'}),
+            'subject_type': Select(choices=SubjectType, attrs={'class': 'form-select'}),
+        }
+
+        labels = {
+            'surname': 'Фамилия',
+            'name': 'Имя',
+            'patronymic': 'Отчество',
+            'post': 'Должность',
+            'passport_data': 'Паспортные данные',
+            'register_of_specialists': 'Номер а национальном реестре специалистов',
+            'ogrn': 'ОГРН',
+            'inn': 'ИНН',
+            'address': 'Адрес',
+            'phone': 'Номер телефона',
+            'legal_name': 'Юридическое название',
+            'details_admin_doc': '',
+            'participant_type': 'Тип участника',
+            'subject_type': 'Тип объекта',
+        }
--- app/views/ParticipantView.py
+++ app/views/ParticipantView.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect, HttpRequest, HttpResponse
 from django.views import View
 from django.shortcuts import render
 from ..models.ParticipantModel import ParticipantModel
-from ..forms.ParticipantForm import ParticipantForm
+from ..forms.ParticipantForm import ParticipantForm, Participant
 from ..models.ProjectModel import ProjectModel
 
 
@@ -44,7 +44,7 @@ class ParticipantView(View):
 
     @staticmethod
     def index(request: HttpRequest) -> HttpResponse:
-        participants = ParticipantModel.objects.all()
+        participants = ParticipantModel.objects.all().order_by('-id')
 
         return render(request, 'participants/index.html', {'participants': participants})
 
@@ -61,3 +61,32 @@ class ParticipantView(View):
         participants = ParticipantModel.objects.all()
 
         return render(request, 'participants/index.html', {'participants': participants})
+
+    @staticmethod
+    def edit(request: HttpRequest, value) -> HttpResponse:
+        participant = ParticipantModel.objects.get(id=value)
+
+        if request.method == 'POST':
+            form = ParticipantForm(request.POST)
+            if form.is_valid():
+                participant.participant_type = form.cleaned_data["participant_type"]
+                participant.subject_type = form.cleaned_data["subject_type"]
+                participant.surname = form.cleaned_data["surname"]
+                participant.name = form.cleaned_data["name"]
+                participant.patronymic = form.cleaned_data["patronymic"]
+                participant.post = form.cleaned_data["post"]
+                participant.passport_data = form.cleaned_data["passport_data"]
+                participant.register_of_specialists = form.cleaned_data["register_of_specialists"]
+                participant.ogrn = form.cleaned_data["ogrn"]
+                participant.inn = form.cleaned_data["inn"]
+                participant.address = form.cleaned_data["address"]
+                participant.phone = form.cleaned_data["phone"]
+                participant.legal_name = form.cleaned_data["legal_name"]
+                participant.details_admin_doc = form.cleaned_data["details_admin_doc"]
+                participant.save()
+
+                return render(request, 'participants/participant.html', {'participant': participant})
+
+        form = Participant(instance=participant)
+
+        return render(request, 'participants/edit.html', {'participant': participant, 'form': form})
