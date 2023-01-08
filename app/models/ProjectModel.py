2cc801f2017e4e14e5aa1024f75611ad753ce87b
Titov Pavel <ptitov@1d61.com>
2023-01-08 23:44:46 +0300

    Add new participants

--- app/models/ProjectModel.py
+++ app/models/ProjectModel.py
@@ -6,10 +6,15 @@ from django.forms.models import model_to_dict
 class ProjectModel(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True)
     builder = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True, related_name="builder")
+    builder_so = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True, related_name="builder_so")
     person_the_construction = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True,
                                                 related_name="construction")
+    person_the_construction_so = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True,
+                                                   related_name="construction_so")
     person_prepares_doc = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True,
                                             related_name="document")
+    person_prepares_doc_so = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True,
+                                               related_name="document_so")
     representative_builder = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True,
                                                related_name="part_represent_builder")
     representative_person_the_construction = models.ForeignKey(ParticipantModel, on_delete=models.SET_NULL, null=True,
@@ -35,10 +40,16 @@ class ProjectModel(models.Model):
         match column:
             case "builder":
                 self.builder = participant
+            case "builder_so":
+                self.builder_so = participant
             case "person_the_construction":
                 self.person_the_construction = participant
+            case "person_the_construction_so":
+                self.person_the_construction_so = participant
             case "person_prepares_doc":
                 self.person_prepares_doc = participant
+            case "person_prepares_doc_so":
+                self.person_prepares_doc_so = participant
             case "representative_builder":
                 self.representative_builder = participant
             case "representative_person_the_construction":
